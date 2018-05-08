28a0cbd8eb6f79cbd9f93fe10986333b69c0e2a8
iliul <liul.stone@gmail.com>
2018-05-08 13:26:25 +0800

    Init Commit
    Signed-off-by: iliul <liul.stone@gmail.com>

---
+++ speedycloud/object_storage/AbstractProductAPI.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace object_storage
+{
+    class AbstractProductAPI
+    {
+        private string access_key;
+        private string secret_key;
+        private string host;
+        private SortedList<string, string> metadata;
+
+        public AbstractProductAPI(string accesskey, string secretkey)
+        {
+            this.host = "http://osc.speedycloud.net";
+            this.access_key = accesskey;
+            this.secret_key = secretkey;
+            this.metadata = new SortedList<string, string>();
+        }
+
+        private string createSignString(HttpWebRequest request, string path)
+        {
+            string sign = request.Method;
+            sign += "\n" + request.Headers.Get("Content-Md5");
+            sign += "\n" + request.ContentType;
+            string date = request.Date.AddHours(-8).GetDateTimeFormats('r')[0].ToString();
+            sign += "\n" + date;
+            foreach (var entry in metadata)
+            {
+                sign += "\n" + entry.Key.ToLower() + ":" + entry.Value;
+            }
+            sign += "\n" + path;
+            return sign;
+        }
+
+        private string createSign(HttpWebRequest request, string path)
+        {
+            HMACSHA1 hmacsha1 = new HMACSHA1();
+            hmacsha1.Initialize();
+            hmacsha1.Key = Encoding.Default.GetBytes(this.secret_key);
+            byte[] final = hmacsha1.ComputeHash(Encoding.Default.GetBytes(createSignString(request, path)));
+            return Convert.ToBase64String(final);
+        }
+
+        public string request(string method, string path)
+        {
+            string serverURL = this.host + path;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverURL);
+            foreach (var entry in metadata)
+            {
+                request.Headers[entry.Key] = entry.Value;
+            }
+            request.Method = method;
+            request.Date = DateTime.UtcNow;
+            request.Headers.Add("Authorization", "AWS " + this.access_key + ":" + createSign(request, path));
+            return this.returnResponse(request); ;
+        }
+
+        public string putData(string method, string url, string data, string type)
+        {
+            string serverURL = this.host + url;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverURL);
+            foreach (var entry in metadata)
+            {
+                request.Headers[entry.Key] = entry.Value;
+            }
+            request.Method = method;
+            request.Date = DateTime.UtcNow;
+            MD5 md5 = new MD5CryptoServiceProvider();
+            if (type == "string")
+            {
+                request.ContentType = "text/plain";
+                request.ContentLength = data.Length;
+                byte[] result = Encoding.Default.GetBytes(data);
+                byte[] output = md5.ComputeHash(result);
+                request.Headers.Set("Content-Md5", Convert.ToBase64String(output));
+                request.Headers.Add("Authorization", "AWS " + this.access_key + ":" + createSign(request, url));
+                Stream stream = request.GetRequestStream();
+                stream.Write(result, 0, result.Length);
+            }
+            else if (type == "file")
+            {
+                FileStream fileStream = File.OpenRead(data);
+                byte[] output = md5.ComputeHash(fileStream);
+                request.ContentLength = fileStream.Length;
+                fileStream.Close();
+                request.Headers.Set("Content-Md5", Convert.ToBase64String(output));
+                request.Headers.Add("Authorization", "AWS " + this.access_key + ":" + createSign(request, url));
+                Stream stream = request.GetRequestStream();
+                byte[] buffer = new byte[4096];
+                FileStream fs = File.OpenRead(data);
+                while (fs.Read(buffer, 0, 4096) > 0)
+                {
+                    stream.Write(buffer, 0, 4096);
+                    stream.Flush();
+                }
+                fs.Close();
+            }
+            else
+            {
+                return "";
+            }
+            return this.returnResponse(request);
+        }
+
+        public string putKeyFromFile(string method, string url, string path)
+        {
+            return putData(method, url, path, "file");
+        }
+
+        public string putKeyFromString(string method, string url, string requestString)
+        {
+            return putData(method, url, requestString, "string");
+        }
+
+        public string putString(string method, string url, string requestString)
+        {
+            return putData(method, url, requestString, "string");
+        }
+
+        public string requestUpdate(string method, string url, string acl)
+        {
+            string serverURL = this.host + url;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverURL);
+            this.metadata.Add("X-Amz-Acl", acl);
+            foreach (var entry in metadata)
+            {
+                request.Headers[entry.Key] = entry.Value;
+            }
+            request.Method = method;
+            request.Date = DateTime.UtcNow;
+            request.Headers.Add("Authorization", "AWS " + this.access_key + ":" + createSign(request, url));
+            return this.returnResponse(request);
+        }
+
+        private string returnResponse(HttpWebRequest request)
+        {
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream streamResponse = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(streamResponse, Encoding.UTF8);
+            string content = "";
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                content += line;
+            }
+            streamResponse.Close();
+            streamReader.Close();
+            response.Close();
+            return content;
+        }
+    }
+}
---
+++ speedycloud/object_storage/SpeedyCloudS3.cs
@@ -0,0 +1,97 @@
+﻿using System;
+namespace object_storage
+{
+    class SpeedyCloudS3 : AbstractProductAPI
+    {
+        public SpeedyCloudS3(string access_key, string secret_key) : base(access_key, secret_key)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            SpeedyCloudS3 s3 = new SpeedyCloudS3("41A6839C70E2E842D3AB3C2B84BCECAB", "04b7cb09bc9be85888b245fee13d3e4e05096e29b83fc583dead9e5e550e16fc");
+            Console.WriteLine(s3.putObjectFromFile("haha", "heihei", @"c:\Logs\Application.evtx"));
+            //Console.WriteLine(s3.putObjectFromString("haha", "hehe", "baka"));
+            Console.WriteLine(s3.updateKeyAcl("haha", "heihei", "public-read"));
+            Console.ReadKey();
+        }
+
+        public string list(string bucket)
+        {
+            return this.request("GET", "/" + bucket);
+        }
+
+        public string createBucket(string bucket)
+        {
+            return this.request("PUT", "/" + bucket);
+        }
+
+        public string deleteBucket(string bucket)
+        {
+            return this.request("DELETE", "/" + bucket);
+        }
+
+        public string queryBucketAcl(string bucket)
+        {
+            return this.request("GET", string.Format("/{0}?acl", bucket));
+        }
+
+        public string queryObjectAcl(string bucket, string key)
+        {
+            return this.request("GET", string.Format("/{0}/{1}?acl", bucket, key));
+        }
+
+        public string deleteKey(string bucket, string key)
+        {
+            return this.request("DELETE", string.Format("/{0}/{1}", bucket, key));
+        }
+
+        public string deleteVersioningKey(string bucket, string key, string versionId)
+        {
+            return this.request("DELETE", string.Format("/{0}/{1}?versionId={2}", bucket, key, versionId));
+        }
+
+        public string getKeyVersions(string bucket)
+        {
+            return this.request("GET", string.Format("/{0}?versions", bucket));
+        }
+
+        public string configureBucketVersioning(string bucket, string status)
+        {
+            string path = string.Format("/{0}?versioning", bucket);
+            string versioningBody = string.Format("<?xml version=\"1.0\"encoding=\"UTF-8\"?><VersioningConfiguration xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\"><Status>{0}</Status ></VersioningConfiguration>", status);
+            return this.putString("PUT", path, versioningBody);
+        }
+
+        public string getBucketVersioningStatus(string bucket)
+        {
+            return this.request("GET", string.Format("/{0}?versioning", bucket));
+        }
+
+        public string putObjectFromFile(string bucket, string key, string path)
+        {
+            return this.putKeyFromFile("PUT", string.Format("/{0}/{1}", bucket, key), path);
+        }
+
+        public string putObjectFromString(string bucket, string key, string s)
+        {
+            return this.putKeyFromString("PUT", string.Format("/{0}/{1}", bucket, key), s);
+        }
+
+        public string updateBucketAcl(string bucket, string key, string acl)
+        {
+            return this.requestUpdate("PUT", string.Format("/{0}?acl", bucket), acl);
+        }
+
+        public string updateKeyAcl(string bucket, string key, string acl)
+        {
+            return this.requestUpdate("PUT", string.Format("/{0}/{1}?acl", bucket, key), acl);
+        }
+
+        public string updateVersioningKeyAcl(string bucket, string key, string versionId, string acl)
+        {
+            return this.requestUpdate("PUT", string.Format("/{0}/{1}?acl&versionId={2}", bucket, key, versionId), acl);
+        }
+    }
+}
